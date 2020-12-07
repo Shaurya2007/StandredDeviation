33f46bdd7f1b523aabb72685c443b144ee6960af
Shaurya2007 <57561094+Shaurya2007@users.noreply.github.com>
2020-12-07 14:55:07 +0530

    Add files via upload

---
+++ StanDev.py
@@ -0,0 +1,33 @@
+import math
+import csv
+
+with open('data.csv',newline='') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+data=file_data[0]
+
+def mean(data):
+    n = len(data)
+    total = 0 
+    for x in data:
+        total+= int(x)
+
+    mean = total/n
+    return mean
+
+square_list = []
+for num in data :
+    a = int(num) - mean(data)
+    a = a**2
+    square_list.append(a)
+    print(square_list)
+
+sum=0
+for i in square_list:
+    sum = sum + i
+    print(sum)
+
+result = sum/ (len(data)-1)
+std_deviation = math.sqrt(result)
+print(std_deviation)
